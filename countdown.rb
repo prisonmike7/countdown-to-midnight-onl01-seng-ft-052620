9498cea171ebdbe1bb08a2971252f49eaad28692
Michael Harris <mwharris7@gmail.com>
2020-05-28 00:56:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(start_num)
+
+  while start_num > 0
+    puts "#{start_num} SECONDS(S)!"
+    start_num -= 1
+  end
+  
+  return "HAPPY NEW YEAR!"
 end
